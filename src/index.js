17b264fb25ca62fb5518bcab9918b0ffe4b7619e
MigalenkoVadim <36640684+MigalenkoVadim@users.noreply.github.com>
2018-02-26 16:31:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr = equation.split(' ');
+var a = arr[0];
+for (var i = 0; i < arr.length; i++) {
+    if ( arr[i] == "x") {
+        var b = +(arr[i-3] + arr[i-2])
+    }
+}
+for (var i = 0; i < arr.length; i++) {
+    if ( arr[i] == "x") {
+        var c = +(arr[i+1] + arr[i+2])
+    }
+}
+var D = b*b - 4*a*c;
+var answer = [];
+answer.push(Math.round(-b+Math.sqrt(D))/(2*a));
+answer.push(Math.round(-b-Math.sqrt(D))/(2*a));
+answer.sort(compareNumeric);
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
+return answer;
 }
